34ee581fc8d2d6aa3f20ebd96fd7b77850f49d58
Manoj V Sarangmath <manoj.kumar.v.sarangmath@sap.com>
2019-01-29 20:19:28 +0530

    my-first-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlert } from './SuccessAlert/successAlert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlert,
+    WarningAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
